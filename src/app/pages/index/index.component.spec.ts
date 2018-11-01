adecbc0818f8e1f05dd1c6d6935c4c05204cfabb
Maximo Sojo <maxsojo13@gmail.com>
2018-10-31 22:29:31 -0400

    refactor(Test): Se registra y actualizan test.

---
+++ src/app/pages/index/index.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed, async } from '@angular/core/testing';
+import { IndexComponent } from './index.component';
+describe('IndexComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        IndexComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(IndexComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+  it(`should have as title 'maxtoan'`, async(() => {
+    const fixture = TestBed.createComponent(IndexComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('@maxtoan');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(IndexComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('@maxtoan');
+  }));
+});
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+	title = "@maxtoan";
+	
+  	constructor() { }
 
-  ngOnInit() {
-  }
-
+  	ngOnInit() {
+  	}
 }
