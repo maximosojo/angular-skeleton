aa0bae4fb83f15dd4c2a8966ea9dd667eaac2222
Maximo Sojo <maxsojo13@gmail.com>
2018-11-19 11:38:13 -0400

    refactor(Node): Se registra incio servidor de node.

---
+++ server.js
@@ -0,0 +1,29 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+	res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || 8000;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`Client running on localhost:${port}`));
